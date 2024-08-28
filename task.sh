20b921a1e84d02663ff8c5d443a0b84b476f1499
Андрей Владимиров <Andrey.Vladimirov.ya@yandex.ru>
2024-08-28 17:19:36 +0300

    3 commit

--- task.sh
+++ task.sh
@@ -6,13 +6,11 @@
 #   dir2
 #   dir3
 #       dir4
-mkdir -p /task/dir1
-mkdir /task/dir2
-mkdir -p /task/dir3/dir4
+mkdir -p task/dir1 task/dir2 task/dir3/dir4
 
 # изменяем текущую директорию на task
 
-cd /task
+cd task
 
 # создаём пустой файл task/dir2/empty
 
@@ -24,8 +22,8 @@ touch /task/dir2/empty
 
 
 touch /task/dir2/hello.sh
-echo '#!/bin/bash' > /task/dir2/hello.sh
-echo 'echo "$1, привет!"' >> /task/dir2/hello.sh
+echo '#!/bin/bash
+echo "$1, привет!"'
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 
@@ -38,12 +36,12 @@ ls /task/dir2 > /task/dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
 
-cp -r -T /task/dir2 task/dir3/dir4
+cp -r /task/dir2/* task/dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
 
-find /task -name "*.txt" > /task/dir1/summary.txt
+find . -name "*.txt" > /task/dir1/summary.txt
 
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
@@ -52,13 +50,13 @@ find /task/dir2/list.txt >> task/dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
 
-NAME='Всем студентам'
+export NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
 
 chmod +x task/dir2/hello.sh
-/task/dir2/hello.sh $NAME >> /task/dir1/summary.txt
+/task/dir2/hello.sh "$NAME" >> /task/dir1/summary.txt
 
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
@@ -72,15 +70,15 @@ cat /task/Практическое задание
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
 
-grep -n "dir" /task/Практическое задание
+grep | sort -n "dir" /task/Практическое задание
 
 
 # меняем текущую директорию на родительскую для task
 
 
-cd
+cd ..
 
 # удаляем директорию task со всем содержимым
 
-rm -rf /task
+rm -r task
 
